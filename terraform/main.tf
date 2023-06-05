8a6a54554eb2210b58f2de3a864d7d6e1cdfc2c2
s13tc2 <s13tc2@protonmail.com>
2023-06-05 00:48:12 -0400

    edit docker-compose

--- terraform/main.tf
+++ terraform/main.tf
@@ -158,12 +158,11 @@ cd nyc-neighborhood-transportation
 echo 'Setup Airflow environment variables'
 echo "
 AIRFLOW_CONN_POSTGRES_DEFAULT=postgres://airflow:airflow@localhost:5439/airflow
-AIRFLOW_CONN_AWS_DEFAULT=aws://?region_name=${var.aws_region}
+AIRFLOW_CONN_MY_AWS_CONN=aws://?region_name=${var.aws_region}
 AIRFLOW_VAR_BUCKET=${aws_s3_bucket.sde-data-lake.id}
 " > env
 
 echo 'Start Airflow containers'
-make up
 
 echo "-------------------------END SETUP---------------------------"
 
